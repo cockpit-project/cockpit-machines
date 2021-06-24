7bbaab62719acd37760f2b78005f2c60e673d073
Katerina Koukiou <kkoukiou@redhat.com>
2021-06-24 17:48:42 +0200

    tests: make sure package.json is ignored when installing test deps

--- test/browser/run-test.sh
+++ test/browser/run-test.sh
@@ -9,7 +9,10 @@ make bots test/common test/reference
 
 # support running from clean git tree
 if [ ! -d node_modules/chrome-remote-interface ]; then
+    # copy package.json temporarilly otherwise npm might try to install the dependencies from it
+    mv package.json .package.json
     npm install chrome-remote-interface sizzle
+    mv .package.json package.json
 fi
 
 # disable detection of affected tests; testing takes too long as there is no parallelization,
