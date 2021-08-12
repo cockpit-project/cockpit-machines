cockpit-project/cockpit-machines

commit 1c286554f3b7f05441588822aefcdc0a92bed0e3

Rewrite create/install VM scripts to python
The only functional change is that the xlmns tag for libosinfo and
cockpit_machines is now added to the `metadata` element itself
and not its children.

src/scripts/install_machine.py
@@ -1,0 +1,331 @@
+#! /usr/bin/python3
+
+from contextlib import contextmanager
+import traceback
+import json
+import subprocess
+import sys
+import os
+import xml.etree.ElementTree as ET
+import tempfile
+import logging
+
+
+def virsh(connection, *args):
+    cmd = ("virsh", "-c", f"qemu:///{connection}") + args
+    logging.debug("Running virsh command :" + ' '.join(cmd))
+    return subprocess.check_output(cmd)
+
+
+def assert_vm_exists(connection, name):
+    # This function should never raise Exception
+    try:
+        subprocess.check_call(["virsh", "-c", f"qemu:///{connection}", "domuuid", name])
+    except subprocess.CalledProcessError:
+        logging.error("VM disappeared while being created")
+        raise
+
+
+def get_graphics_capabilies(connection):
+    capabilities = virsh(connection, 'domcapabilities')
+
+    root = ET.ElementTree(ET.fromstring(capabilities))
+    graphics = root.find('devices').find('graphics')
+    supported = graphics.get('supported')
+
+    consoles = []
+    if supported == 'yes':
+        for value in graphics.find('enum').findall('value'):
+            consoles.append(value.text)
+
+    logging.debug('get_graphics_capabilies: ' + ', '.join(consoles))
+
+    return [c for c in consoles if c in ['vnc', 'spice']]
+
+
+def prepare_graphics_params(connection):
+    params = []
+
+    graphics_cap = get_graphics_capabilies(connection)
+    if graphics_cap:
+        for graphics in graphics_cap:
+            params += ['--graphics', f"{graphics},listen=127.0.0.1"]
+    else:
+        params += ['--graphics', 'none']
+    return params
+
+
+@contextmanager
+def prepare_unattended(args):
+    params = []
+    if args['type'] == 'create' and args['unattended']:
+        params.append("--unattended")
+
+        unattended_params = [f"profile={args['profile']}"]
+
+        if args['rootPassword']:
+            root_pass_file = tempfile.NamedTemporaryFile(
+                prefix="cockpit-machines-",
+                suffix="-admin-password",
+                mode='w+'
+            )
+            root_pass_file.write(args['rootPassword'])
+            root_pass_file.flush()
+            unattended_params.append(f"admin-password-file={root_pass_file.name}")
+
+        if args['userLogin']:
+            unattended_params.append(f"user-login={args['userLogin']}")
+
+        if args['userPassword']:
+            user_pass_file = tempfile.NamedTemporaryFile(
+                prefix="cockpit-machines-",
+                suffix="-user-password",
+                mode='w+'
+            )
+            user_pass_file.write(args['userPassword'])
+            user_pass_file.flush()
+            unattended_params.append(f"user-password-file={user_pass_file.name}")
+
+        params.append(",".join(unattended_params))
+
+    yield params
+
+
+@contextmanager
+def prepare_cloud_init(args):
+    params = []
+    if args['type'] == 'create' and args['sourceType'] == 'cloud':
+        params.append("--cloud-init")
+        user_data_file = tempfile.NamedTemporaryFile(
+            prefix="cockpit-machines-",
+            suffix="-user-data",
+            mode='w+'
+        )
+        user_data_file.write("#cloud-config\n")
+        if 'userLogin' in args:
+            user_data_file.write("users:\n")
+            user_data_file.write(f"  - name: {args['userLogin']}\n")
+
+        if 'rootPassword' in args or 'userPassword' in args:
+            user_data_file.write("chpasswd:\n")
+            user_data_file.write("  list: |\n")
+            if 'rootPassword' in args:
+                user_data_file.write(f"    root:{args['rootPassword']}\n")
+            if 'userPassword' in args:
+                user_data_file.write(f"    {args['userLogin']}:{args['userPassword']}\n")
+            user_data_file.write("  expire: False\n")
+
+        user_data_file.flush()
+        params.append(f"user-data={user_data_file.name}")
+
+    yield params
+
+
+def prepare_installation_source(args):
+    params = []
+    only_define = args['type'] == 'create' and not args['startVm']
+    if only_define:
+        params.append("--print-xml")
+
+    if args['sourceType'] == "pxe":
+        params += ['--pxe', '--network', args['source']]
+    elif args['sourceType'] == "os":
+        params += ['--install', f"os={args['os']}"]
+    elif args['sourceType'] in ['disk_image', 'cloud']:
+        params.append("--import")
+    elif ((args['source'][0] == '/' and os.path.isfile(args['source'])) or
+            (args['sourceType'] == 'url' and args['source'].endswith(".iso"))):
+        if not only_define:
+            params += ['--cdrom', args['source']]
+    else:
+        if not only_define:
+            params += ['--location', args['source']]
+
+    return params
+
+
+@contextmanager
+def prepare_virt_install_params(args):
+    logging.debug(args)
+    with prepare_unattended(args) as unattended_params, prepare_cloud_init(args) as cloud_init_params:
+        params = [
+            "virt-install",
+            "--connect", f"qemu:///{args['connectionName']}",
+            "--quiet",
+            "--name", args['vmName'],
+            "--os-variant", args['os'],
+            "--memory", str(args['memorySize']),
+        ]
+
+        if args['type'] == 'install' or ('storagePool' in args and args['storagePool'] != 'NewVolume'):
+            params += ["--check", "path_in_use=off"]
+
+        if args['sourceType'] != 'disk_image':
+            params += ["--wait", "-1"]
+
+        if args['type'] == 'install' or args['startVm']:
+            params.append("--noautoconsole")
+
+        # Disks
+        if args['type'] == 'install':
+            disks = args['disks'] or ['none']
+            for disk in disks:
+                params += ['--disk', disk]
+        else:
+            params.append("--disk")
+
+            if args['sourceType'] == 'disk_image':
+                disk = f"{args['source']},device=disk"
+            elif args['storagePool'] == 'NoStorage':
+                disk = "none"
+            else:
+                if args['storagePool'] != 'NewVolume':
+                    disk = f"vol={args['storagePool']}/{args['storageVolume']}"
+                else:
+                    disk = f"size={args['storageSize']},format=qcow2"
+                if args['sourceType'] == "cloud":
+                    disk += f",backing_store={args['source']}"
+            params.append(disk)
+
+        # NICs
+        if args['type'] == "install":
+            vnics = args['vnics'] or ['none']
+            for network in vnics:
+                params += ['--network', network]
+
+        # Consoles
+        if args['type'] == "install":
+            graphics = args['graphics'] or ['none']
+            for console in graphics:
+                params += ['--graphics', console]
+        else:
+            params += prepare_graphics_params(args['connectionName'])
+
+        # Installation media
+        if args['sourceType'] != "cloud":
+            params += prepare_installation_source(args)
+
+        # Autostart
+        if args['type'] == 'install' and args['autostart']:
+            params.append("--autostart")
+
+        # VCPUs
+        if 'vcpu' in args:
+            params += ['--vcpus', args['vcpu']]
+
+        # Firmware
+        if 'firmware' in args:
+            params += ['--boot', args['firmware']]
+
+        params += unattended_params
+        params += cloud_init_params
+
+        logging.debug(params)
+
+        yield params
+
+
+def create_vm(args):
+    with prepare_virt_install_params(args) as params:
+        xml = subprocess.check_output(params)
+
+    if args['startVm']:
+        try:
+            assert_vm_exists(args['connectionName'], args['vmName'])
+
+            xml = virsh(args['connectionName'], "dumpxml", "--inactive", args['vmName'])
+        except subprocess.CalledProcessError:
+            logging.info("The VM got deleted while being installed")
+            logging.info(traceback.format_exc())
+            sys.exit(0)
+
+    xml_last_phase = xml.strip().split(b'\n\n')[-1]
+    # Get last step only - virt-install can output 1 or 2 steps
+    inject_metadata(xml_last_phase.decode())
+
+
+def install_vm(args):
+    # If the VM already exists and we want to run the installer we need to remove it first
+    try:
+        virsh(args['connectionName'], "destroy", args['vmName'])
+    except subprocess.CalledProcessError:
+        pass
+
+    prevXML = virsh(args['connectionName'], "dumpxml", args['vmName'])
+
+    virsh(args['connectionName'], "undefine", args['vmName'], "--managed-save")
+
+    with prepare_virt_install_params(args) as params:
+        try:
+            subprocess.check_output(params)
+        except subprocess.CalledProcessError as e:
+            # If virt-install returned non-zero return code, redefine
+            # the VM so that we get back the metadata which enable the 'Install'
+            # button, so that the user can re-attempt installation
+            with tempfile.NamedTemporaryFile() as file:
+                logging.debug("virt-install failed, redefining to renable the 'Install' button")
+                file.write(prevXML)
+                file.flush()
+                virsh(args['connectionName'], "define", file.name)
+            raise e
+
+        assert_vm_exists(args['connectionName'], args['vmName'])
+
+        xml = virsh(args['connectionName'], "dumpxml", "--inactive", args['vmName'])
+
+        inject_metadata(xml.decode())
+
+
+def inject_metadata(xml):
+    # ET.fromstring() already wants UTF-8 encoded bytes
+    root = ET.fromstring(xml)
+    metadata = root.find('metadata')
+    cockpit_machines_metadata = metadata.find('cockpit_machines')
+    if cockpit_machines_metadata:
+        metadata.remove(cockpit_machines_metadata)
+
+    has_install_phase = "true"
+    # VM does not have a pending install phase (visible Install button in the UI) if:
+    # - The script is called from the 'Install' button
+    # - The script is called from the 'Create' dialog and the VM is started (installer will run)
+    # - The script is called from the 'Import' dialog
+    if args['type'] == 'install' or args['startVm'] or args['sourceType'] == 'disk_image':
+        has_install_phase = "false"
+
+    # Register used namespaces
+    ET.register_namespace("cockpit_machines", "https://github.com/cockpit-project/cockpit-machines")
+    ET.register_namespace("libosinfo", "http://libosinfo.org/xmlns/libvirt/domain/1.0")
+
+    METADATA = f'''
+<cockpit_machines:data xmlns:cockpit_machines="https://github.com/cockpit-project/cockpit-machines"> \
+  <cockpit_machines:has_install_phase>{has_install_phase}</cockpit_machines:has_install_phase> \
+  <cockpit_machines:install_source_type>{args['sourceType']}</cockpit_machines:install_source_type> \
+  <cockpit_machines:install_source>{args['source']}</cockpit_machines:install_source> \
+  <cockpit_machines:os_variant>{args['os']}</cockpit_machines:os_variant> \
+</cockpit_machines:data>
+'''
+
+    cockpit_machines_metadata_new = ET.fromstring(METADATA)
+    metadata.append(cockpit_machines_metadata_new)
+
+    updated_xml = ET.tostring(root)
+
+    with tempfile.NamedTemporaryFile() as file:
+        file.write(updated_xml)
+        file.flush()
+        virsh(args['connectionName'], "define", file.name)
+
+
+logging.basicConfig(level=logging.ERROR, format='%(message)s')
+logging.debug(sys.argv[1])
+
+args = json.loads(sys.argv[1], strict=False)
+
+logging.debug(args)
+
+if args['type'] == 'create':
+    create_vm(args)
+elif args['type'] == 'install':
+    install_vm(args)
+else:
+    raise NotImplementedError("unknown type " + args['type'])

src/libvirt-common.js
@@ -1,7 +1,6 @@
 import cockpit from 'cockpit';
 import * as service from 'service.js';
-import createVmScript from 'raw-loader!./scripts/create_machine.sh';
-import installVmScript from 'raw-loader!./scripts/install_machine.sh';
+import installVmScript from 'raw-loader!./scripts/install_machine.py';
 
 import * as python from "python.js";
 import getOSListScript from 'raw-loader!./getOSList.py';
@@ -39,6 +38,8 @@ import store from './store.js';
 import VMS_CONFIG from './config.js';
 
 const METADATA_NAMESPACE = "https://github.com/cockpit-project/cockpit-machines";
+
+let pythonPath;
 
 export function buildConsoleVVFile(consoleDetail) {
     return '[virt-viewer]\n' +
@@ -1101,7 +1102,8 @@ export function createVm({
     if (connectionName === 'system')
         opts.superuser = 'try';
 
-    return cockpit.script(createVmScript, [
+    const args = JSON.stringify({
+        type: "create",
         connectionName,
         vmName,
         source,
@@ -1117,7 +1119,11 @@ export function createVm({
         userPassword,
         userLogin,
         profile,
-    ], opts)
+    });
+
+    return cockpit
+            .spawn([pythonPath, "--", "-", args], opts)
+            .input(installVmScript)
             .then(() => {
                 finishVmCreateInProgress(vmName, connectionName);
                 clearVmUiState(vmName, connectionName);
@@ -1141,7 +1147,11 @@ function getLoggedInUser() {
     });
 }
 
-function getOsInfoList () {
+function getPythonPath() {
+    return cockpit.spawn(["/bin/sh", "-c", "which /usr/libexec/platform-python 2>/dev/null || which python3 2>/dev/null || which python"]).then(pyexe => { pythonPath = pyexe.trim() });
+}
+
+function getOsInfoList() {
     logDebug(`GET_OS_INFO_LIST():`);
     return python.spawn(getOSListScript, null, { err: "message", environ: ['LC_ALL=C.UTF-8'] })
             .then(osList => {
@@ -1154,6 +1164,7 @@ function getOsInfoList () {
 }
 
 export function initState() {
+    getPythonPath();
     getLoggedInUser();
     getOsInfoList();
 }
@@ -1174,20 +1185,25 @@ export function installVm({ onAddErrorNotification, vm }) {
     if (connectionName === 'system')
         opts.superuser = 'try';
 
-    return cockpit.script(installVmScript, [
-        connectionName,
-        name,
-        metadata.installSourceType,
-        metadata.installSource,
-        metadata.osVariant,
-        prepareMemoryParam(convertToUnit(currentMemory, units.KiB, units.MiB), convertToUnit(memory, units.KiB, units.MiB)),
-        prepareVcpuParam(vcpus, cpu),
-        prepareDisksParam(disks),
-        prepareDisplaysParam(displays),
-        prepareNICParam(interfaces),
-        firmware == "efi" ? 'uefi' : '',
+    const args = JSON.stringify({
         autostart,
-    ], opts)
+        connectionName,
+        disks: prepareDisksParam(disks),
+        firmware: firmware == "efi" ? 'uefi' : '',
+        graphics: prepareDisplaysParam(displays),
+        memorySize: prepareMemoryParam(convertToUnit(currentMemory, units.KiB, units.MiB), convertToUnit(memory, units.KiB, units.MiB)),
+        os: metadata.osVariant,
+        source: metadata.installSource,
+        sourceType: metadata.installSourceType,
+        type: "install",
+        vcpu: prepareVcpuParam(vcpus, cpu),
+        vmName: name,
+        vnics: prepareNICParam(interfaces),
+    });
+
+    return cockpit
+            .spawn([pythonPath, "--", "-", args], opts)
+            .input(installVmScript)
             .finally(() => clearVmUiState(name, connectionName));
 }
 

src/libvirtUtils.js
@@ -42,12 +42,13 @@ function prepareParamsFromObj(dataElem, valueTransformer) {
 }
 
 function prepareParamsFromArrOfObjs(arrayData, valueTransformer) {
-    return arrayData.map(dataElem => prepareObj(dataElem, valueTransformer)).join("\n");
+    return arrayData.map(dataElem => prepareObj(dataElem, valueTransformer))
+            .filter(Boolean);
 }
 
 function prepareParamsFromObjOfObjs(objectData, valueTransformer) {
     return Object.keys(objectData).map(key => prepareObj(objectData[key], valueTransformer))
-            .join("\n");
+            .filter(Boolean);
 }
 
 export function prepareDisplaysParam(displays) {

src/scripts/create_machine.sh
@@ -1,230 +1,0 @@
-#!/bin/sh
-set -eu -o noglob
-
-CONNECTION_URI="qemu:///$1" # example: qemu:///system
-VM_NAME="$2"
-SOURCE="$3"
-SOURCE_TYPE="$4"
-OS="$5"
-MEMORY_SIZE="$6" # in MiB
-STORAGE_SIZE="$7" # in GiB
-START_VM="$8"
-STORAGE_POOL="$9"
-STORAGE_VOLUME="${10}"
-UNATTENDED="${11}"
-ROOT_PASSWORD="${12}"
-USER_PASSWORD="${13}"
-USER_LOGIN="${14}"
-PROFILE="${15}"
-
-vmExists(){
-   virsh -c "$CONNECTION_URI" list --all | awk  '{print $2}' | grep -q --line-regexp --fixed-strings "$1"
-}
-
-err_handler () {
-    rm -f "$ROOT_PASSWORD_FILE" "$XMLS_FILE" "$USER_DATA_FILE"
-}
-
-handleFailure(){
-    exit $1
-}
-
-trap err_handler EXIT
-
-XMLS_FILE="`mktemp`"
-ROOT_PASSWORD_FILE="`mktemp`"
-USER_PASSWORD_FILE="`mktemp`"
-USER_DATA_FILE="`mktemp`"
-
-if [ "$UNATTENDED" = "true" ]; then
-    if [ -z "$PROFILE" ]; then
-        PROFILE_PARAM=""
-    else
-        PROFILE_PARAM="profile=$PROFILE"
-    fi
-    if [ -z "$ROOT_PASSWORD" ]; then
-        ROOT_PASSWORD_PARAM=''
-    else
-        echo "$ROOT_PASSWORD" > "$ROOT_PASSWORD_FILE"
-        ROOT_PASSWORD_PARAM=",admin-password-file=$ROOT_PASSWORD_FILE"
-    fi
-    if [ -z "$USER_PASSWORD" ]; then
-        USER_PASSWORD_PARAM=''
-    else
-        echo "$USER_PASSWORD" > "$USER_PASSWORD_FILE"
-        USER_PASSWORD_PARAM=",user-password-file=$USER_PASSWORD_FILE"
-    fi
-    if [ -z "$USER_LOGIN" ]; then
-        USER_LOGIN_PARAM=""
-    else
-        USER_LOGIN_PARAM=",user-login=$USER_LOGIN"
-    fi
-    UNATTENDED_PARAMS="--unattended $PROFILE_PARAM$ROOT_PASSWORD_PARAM$USER_PASSWORD_PARAM$USER_LOGIN_PARAM"
-else
-    UNATTENDED_PARAMS=""
-fi
-
-if [ "$SOURCE_TYPE" = "cloud" ]; then
-    if [ -z "$USER_PASSWORD" ]; then
-        USER_LINE=""
-        CREATE_USER_LINE=""
-    else
-        USER_LINE="    $USER_LOGIN:$USER_PASSWORD"
-        CREATE_USER_LINE="users:"$'\n'"  - name: $USER_LOGIN"
-    fi
-    if [ -z "$ROOT_PASSWORD" ]; then
-        ROOT_LINE=""
-    else
-        ROOT_LINE="    root:$ROOT_PASSWORD"$'\n'
-    fi
-    cat <<EOT >> $USER_DATA_FILE
-#cloud-config
-$CREATE_USER_LINE
-chpasswd:
-  list: |
-$ROOT_LINE$USER_LINE
-  expire: False
-EOT
-    CLOUD_INIT_PARAMS="--cloud-init user-data=$USER_DATA_FILE"
-else
-    CLOUD_INIT_PARAMS=""
-fi
-
-# prepare virt-install parameters
-if [ "$SOURCE_TYPE" = "disk_image" ]; then
-    DISK_OPTIONS="'$SOURCE',device=disk"
-else
-    if [ "$STORAGE_POOL" = "NoStorage" ]; then
-        # default to no disk if size 0
-        DISK_OPTIONS="none"
-    elif [ "$STORAGE_POOL" != "NewVolume" ]; then
-        DISK_OPTIONS="vol='$STORAGE_POOL/$STORAGE_VOLUME'"
-    else
-        DISK_OPTIONS="size=$STORAGE_SIZE,format=qcow2"
-    fi
-    if [ "$SOURCE_TYPE" = "cloud" ]; then
-        DISK_OPTIONS="$DISK_OPTIONS,backing_store='$SOURCE'"
-    fi
-fi
-
-DOM_GRAPHICS_CAPABILITIES="$(virsh domcapabilities | awk "/<graphics supported='yes'/ {flag=1; next}; /<\/graphics/ {flag=0}; flag")"
-GRAPHICS_PARAM=""
-if echo "$DOM_GRAPHICS_CAPABILITIES" | grep -q vnc; then
-    GRAPHICS_PARAM="--graphics vnc,listen=127.0.0.1"
-fi
-if echo "$DOM_GRAPHICS_CAPABILITIES" | grep -q spice; then
-    GRAPHICS_PARAM="--graphics spice,listen=127.0.0.1 $GRAPHICS_PARAM"
-fi
-if [ -z "$GRAPHICS_PARAM" ]; then
-    GRAPHICS_PARAM="--graphics none"
-fi
-
-if [ "$SOURCE_TYPE" = "pxe" ]; then
-    INSTALL_METHOD="--pxe --network $SOURCE"
-elif [ "$SOURCE_TYPE" = "os" ]; then
-    INSTALL_METHOD="--install os=$OS"
-elif [ "$START_VM" = "true" ]; then
-    if [ $SOURCE_TYPE = "cloud" ]; then
-        INSTALL_METHOD=""
-    elif [ "$SOURCE_TYPE" = "disk_image" ]; then
-        INSTALL_METHOD="--import"
-    elif ( [ "${SOURCE#/}" != "$SOURCE" ] && [ -f "${SOURCE}" ] ) || ( [ "$SOURCE_TYPE" = "url" ] && [ "${SOURCE%.iso}" != "$SOURCE" ] ); then
-        INSTALL_METHOD="--cdrom '$SOURCE'"
-    else
-        INSTALL_METHOD="--location '$SOURCE'"
-    fi
-else
-    # prevents creating duplicate cdroms if start vm is false
-    # or if no source received
-    INSTALL_METHOD=""
-fi
-
-if [ "$START_VM" = "true" ]; then
-    STARTUP_PARAMS="--noautoconsole"
-    HAS_INSTALL_PHASE="false"
-    # Wait for the installer to complete in case we don't use existing image
-    if [ "$SOURCE_TYPE" != "disk_image" ]; then
-        STARTUP_PARAMS="$STARTUP_PARAMS --wait -1"
-    fi
-else
-    # 2 = last phase only
-    STARTUP_PARAMS="--print-xml"
-    # Installation options that don't have install phase should unset the
-    # HAS_INSTALL_PHASE to prevent the Install button from being shown in
-    # the UI.
-    if [ "$SOURCE_TYPE" = "disk_image" ]; then
-        HAS_INSTALL_PHASE="false"
-    else
-        HAS_INSTALL_PHASE="true"
-    fi
-fi
-
-
-if [ "$STORAGE_POOL" != "NewVolume" ]; then
-    CHECK_PARAM="--check path_in_use=off"
-else
-    CHECK_PARAM=""
-fi
-
-eval virt-install \
-    --connect "$CONNECTION_URI" \
-    --name "$VM_NAME" \
-    --os-variant "$OS" \
-    --memory "$MEMORY_SIZE" \
-    --quiet \
-    --disk  "$DISK_OPTIONS" \
-    "$CHECK_PARAM" \
-    "$STARTUP_PARAMS" \
-    "$INSTALL_METHOD" \
-    "$GRAPHICS_PARAM" \
-    "$UNATTENDED_PARAMS" \
-    "$CLOUD_INIT_PARAMS" \
-> "$XMLS_FILE" || handleFailure $?
-
-# The VM got deleted while being installed
-if ! $(vmExists "$VM_NAME") && [ "$START_VM" = "true" ]; then
-    exit 0
-fi
-
-# add metadata to domain
-
-if [ "$START_VM" = "true" ]; then
-    vmExists "$VM_NAME" || handleFailure $?
-    virsh -c "$CONNECTION_URI" -q dumpxml --inactive "$VM_NAME" > "$XMLS_FILE"
-fi
-
-# LAST STEP ONLY - virt-install can output 1 or 2 steps
-DOMAIN_MATCHES=`grep -n '</domain>' "$XMLS_FILE"`
-LAST_STEP=`echo "$DOMAIN_MATCHES" | wc -l`
-CURRENT_STEP=1
-START_LINE=1
-
-# go through all domains (line numbers) and increment steps
-echo "$DOMAIN_MATCHES"  |  sed 's/[^0-9]//g' | while read -r FINISH_LINE ; do
-        QUIT_LINE="`expr $FINISH_LINE + 1`"
-        # define only last step
-        if [ "$CURRENT_STEP" = "$LAST_STEP" ]; then
-            sed -n -i "$START_LINE"','"$FINISH_LINE"'p;'"$QUIT_LINE"'q' "$XMLS_FILE"
-            METADATA_LINE=`grep -n '</metadata>' "$XMLS_FILE" | sed 's/[^0-9]//g'`
-
-            METADATA='    <cockpit_machines:data xmlns:cockpit_machines="https://github.com/cockpit-project/cockpit-machines"> \
-      <cockpit_machines:has_install_phase>'"$HAS_INSTALL_PHASE"'</cockpit_machines:has_install_phase> \
-      <cockpit_machines:install_source_type>'"$SOURCE_TYPE"'</cockpit_machines:install_source_type> \
-      <cockpit_machines:install_source>'"$SOURCE"'</cockpit_machines:install_source> \
-      <cockpit_machines:os_variant>'"$OS"'</cockpit_machines:os_variant> \
-    </cockpit_machines:data>'
-
-            if [ -z "$METADATA_LINE"  ]; then
-                METADATA_LINE="`cat "$XMLS_FILE" | wc -l`"
-                METADATA='\ \ <metadata> \
-'"$METADATA"' \
-  </metadata>'
-            fi
-
-            #inject metadata, and define
-            sed "$METADATA_LINE""i $METADATA" "$XMLS_FILE" | virsh -c "$CONNECTION_URI" -q define /dev/stdin || handleFailure $?
-        else
-            START_LINE="$QUIT_LINE"
-            CURRENT_STEP="`expr $CURRENT_STEP + 1`"
-        fi
-done

src/scripts/install_machine.sh
@@ -1,143 +1,0 @@
-#!/bin/sh
-
-set -eu -o noglob
-
-CONNECTION_URI="qemu:///$1" # example: qemu:///system
-VM_NAME="$2"
-SOURCE_TYPE="$3"
-SOURCE="$4"
-OS="$5"
-MEMORY="$6"
-VCPUS="$7"
-DISKS="$8"
-DISPLAYS="$9"
-VNICS="${10}"
-BOOT="${11}"
-AUTOSTART="${12}"
-
-# prepare virt-install parameters
-
-vmExists(){
-   virsh -c "$CONNECTION_URI" list --all | awk  '{print $2}' | grep -q --line-regexp --fixed-strings "$1"
-}
-
-createOptions(){
-    CREATE_OPTIONS_RESULT=""
-	while IFS= read -r PARAM
-    do
-       if [ -n "$PARAM" ]; then
-           CREATE_OPTIONS_RESULT="${CREATE_OPTIONS_RESULT} $2 $PARAM"
-       fi
-    done << EOF
-    $1
-EOF
-}
-
-if [ -z "$DISKS" ]; then
-    DISKS_PARAM="--disk none"
-else
-    createOptions "'$DISKS'" "--disk"
-    DISKS_PARAM="$CREATE_OPTIONS_RESULT"
-fi
-
-if [ -z "$VNICS" ]; then
-    VNICS_PARAM="--network none"
-else
-    createOptions "$VNICS" "--network"
-    VNICS_PARAM="$CREATE_OPTIONS_RESULT"
-fi
-
-if [ -z "$DISPLAYS" ]; then
-    GRAPHICS_PARAM="--graphics none"
-else
-    createOptions "$DISPLAYS" "--graphics"
-    GRAPHICS_PARAM="$CREATE_OPTIONS_RESULT"
-fi
-
-if [ "$SOURCE_TYPE" = "pxe" ]; then
-    INSTALL_METHOD="--pxe"
-elif [ "$SOURCE_TYPE" = "os" ]; then
-    INSTALL_METHOD="--install os=$OS"
-elif ( [ "${SOURCE#/}" != "$SOURCE" ] && [ -f "${SOURCE}" ] ) || ( [ "$SOURCE_TYPE" = "url" ] && [ "${SOURCE%.iso}" != "$SOURCE" ] ); then
-    INSTALL_METHOD="--cdrom '$SOURCE'"
-else
-    INSTALL_METHOD="--location '$SOURCE'"
-fi
-
-if [ "$AUTOSTART" = "true" ]; then
-    AUTOSTART_PARAM="--autostart"
-else
-    AUTOSTART_PARAM=""
-fi
-
-createOptions "$MEMORY" "--memory"
-MEMORY_PARAM="$CREATE_OPTIONS_RESULT"
-
-if [ -z "$VCPUS" ]; then
-    VCPUS_PARAM=""
-else
-    createOptions "$VCPUS" "--vcpus"
-    VCPUS_PARAM="$CREATE_OPTIONS_RESULT"
-fi
-
-if [ -z "$BOOT" ]; then
-    BOOT_PARAM=""
-else
-    BOOT_PARAM="--boot $BOOT"
-fi
-
-# backup
-DOMAIN_FILE="`mktemp`"
-
-virsh -c "$CONNECTION_URI" -q destroy "$VM_NAME" 2>/dev/null || true
-virsh -c "$CONNECTION_URI" -q dumpxml "$VM_NAME" > "$DOMAIN_FILE"
-virsh -c "$CONNECTION_URI" -q undefine "$VM_NAME" --managed-save
-
-handleFailure() {
-    # If virt-install returned non-zero return code, redefine
-    # the VM show that we get back the metadata which enable the 'Install'
-    # button, so that the user can re-attempt installation
-    set +e
-    virsh -c "$CONNECTION_URI" -q define "$DOMAIN_FILE"
-    rm -f "$DOMAIN_FILE"
-    exit $1
-}
-
-eval virt-install \
-    --connect "$CONNECTION_URI" \
-    --name "$VM_NAME" \
-    --os-variant "$OS" \
-    --quiet \
-    --wait -1 \
-    --noautoconsole \
-    --check path_in_use=off \
-    "$MEMORY_PARAM" \
-    "$DISKS_PARAM" \
-    "$INSTALL_METHOD" \
-    "$GRAPHICS_PARAM" \
-    "$VNICS_PARAM" \
-    "$VCPUS_PARAM" \
-    "$BOOT_PARAM" \
-    "$AUTOSTART_PARAM" || handleFailure $?
-
-vmExists "$VM_NAME"
-# set metadata
-virsh -c "$CONNECTION_URI"  -q dumpxml --inactive "$VM_NAME" > "$DOMAIN_FILE"
-METADATA_LINE=`grep -n '</metadata>' "$DOMAIN_FILE" | sed 's/[^0-9]//g'`
-METADATA='    <cockpit_machines:data xmlns:cockpit_machines="https://github.com/cockpit-project/cockpit-machines"> \
-  <cockpit_machines:has_install_phase>false</cockpit_machines:has_install_phase> \
-  <cockpit_machines:install_source_type>'"$SOURCE_TYPE"'</cockpit_machines:install_source_type> \
-  <cockpit_machines:install_source>'"$SOURCE"'</cockpit_machines:install_source> \
-  <cockpit_machines:os_variant>'"$OS"'</cockpit_machines:os_variant> \
-</cockpit_machines:data>'
-
-if [ -z "$METADATA_LINE"  ]; then
-    METADATA_LINE="`cat "$DOMAIN_FILE" | wc -l`"
-    METADATA='\ \ <metadata> \
-'"$METADATA"' \
-/metadata>'
-fi
-
-#inject metadata, and define
-sed "$METADATA_LINE""i $METADATA" "$DOMAIN_FILE" | virsh -c "$CONNECTION_URI" -q define /dev/stdin
-rm -f "$DOMAIN_FILE"

commit 30be9ec37e25affda0f1bdca1225aa330f2d5551

Fix creation of cloud-init user-data file
Now if only root passwd was set, and the user field were left empty we
would get:

users:
  - name:
chpasswd:
  list: |
    root:strongpassword
    :
  expire: False

Which is obviously wrong. We don't need to check if the keys exist, they
always exist if we reach that part of the code, we need to check that
they are not empty.

Note: we do it correctly for other parameters, like unattended installs

This broke in commit 1c286554f3b7f05441588822aefcdc0a92bed0e3

src/scripts/install_machine.py
@@ -129,16 +129,16 @@ def prepare_cloud_init(args):
             mode='w+'
         )
         user_data_file.write("#cloud-config\n")
-        if 'userLogin' in args:
+        if args['userLogin']:
             user_data_file.write("users:\n")
             user_data_file.write(f"  - name: {args['userLogin']}\n")
 
-        if 'rootPassword' in args or 'userPassword' in args:
+        if args['rootPassword'] or args['userPassword']:
             user_data_file.write("chpasswd:\n")
             user_data_file.write("  list: |\n")
-            if 'rootPassword' in args:
+            if args['rootPassword']:
                 user_data_file.write(f"    root:{args['rootPassword']}\n")
-            if 'userPassword' in args:
+            if args['userPassword']:
                 user_data_file.write(f"    {args['userLogin']}:{args['userPassword']}\n")
             user_data_file.write("  expire: False\n")
 