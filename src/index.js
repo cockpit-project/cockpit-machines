3d7f1459de01b0a7f09af8be6c7e70e3961174f4
Jelle van der Waa <jvanderwaa@redhat.com>
2022-01-12 15:02:07 +0100

    Drop patternfly 3 styles everywhere

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
-import 'patternfly/patternfly-cockpit.scss';
+import 'patternfly/patternfly-4-cockpit.scss';
 import 'polyfills'; // once per application
 
 import React from 'react';
