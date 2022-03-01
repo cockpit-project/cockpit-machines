cockpit-project/cockpit-machines

commit ed073898e0cef96774004568e3cd5ab50f325ce3

Delete disks only after VM is successfully undefined and destroyed
Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2031678

src/libvirtApi/domain.js
@@ -325,13 +325,8 @@ export function domainDelete({
     options,
     storagePools
 }) {
-    function destroy() {
-        return call(connectionName, objPath, 'org.libvirt.Domain', 'Destroy', [0], { timeout, type: 'u' });
-    }
-
-    function undefine() {
+    function deleteStorage() {
         const storageVolPromises = [];
-        const flags = Enum.VIR_DOMAIN_UNDEFINE_MANAGED_SAVE | Enum.VIR_DOMAIN_UNDEFINE_SNAPSHOTS_METADATA | Enum.VIR_DOMAIN_UNDEFINE_NVRAM;
 
         for (let i = 0; i < options.storage.length; i++) {
             const disk = options.storage[i];
@@ -369,14 +364,23 @@ export function domainDelete({
             }
         }
 
-        return Promise.all(storageVolPromises)
-                .then(() => {
-                    return call(connectionName, objPath, 'org.libvirt.Domain', 'Undefine', [flags], { timeout, type: 'u' });
-                });
+        return Promise.all(storageVolPromises);
+    }
+
+    function destroy() {
+        return call(connectionName, objPath, 'org.libvirt.Domain', 'Destroy', [0], { timeout, type: 'u' });
+    }
+
+    function undefine() {
+        const flags = Enum.VIR_DOMAIN_UNDEFINE_MANAGED_SAVE | Enum.VIR_DOMAIN_UNDEFINE_SNAPSHOTS_METADATA | Enum.VIR_DOMAIN_UNDEFINE_NVRAM;
+
+        return call(connectionName, objPath, 'org.libvirt.Domain', 'Undefine', [flags], { timeout, type: 'u' });
     }
 
     if (options.destroy) {
-        return undefine().then(destroy());
+        return destroy()
+                .then(undefine)
+                .then(deleteStorage);
     } else {
         return undefine()
                 .catch(ex => {

commit de37b4868422b24bc48c6c52f8cf9c26452b2c2a

Delete storage files also when the VM is shut off
Commit ed073898e broke this functionality.

Fixes #598

src/libvirtApi/domain.js
@@ -389,7 +389,8 @@ export function domainDelete({
                     // Transient domains get undefined after shut off
                     if (!ex.message.includes("Domain not found"))
                         return Promise.reject(ex);
-                });
+                })
+                .then(deleteStorage);
     }
 }
 

test/check-machines-lifecycle
@@ -444,6 +444,27 @@ class TestMachinesLifecycle(VirtualMachinesCase):
         self.waitVmRow(name, 'system', False)
         b.wait_not_present(f'#vm-{name}-state button:contains("view more")')
 
+        # Delete a shut-off guest and verify the storage was removed
+        name = "vm-shutoff"
+        img2 = f"/var/lib/libvirt/images/{name}-2.img"
+        self.createVm(name, running=False)
+        # Guest disk image exists
+        m.execute(f"while ! test -f {img2}; do sleep 1; done")
+
+        self.performAction(name, "delete")
+
+        b.wait_visible(f"#vm-{name}-delete-modal-dialog .disk-source-file:contains({img2})")
+        b.wait_visible(f"#vm-{name}-delete-modal-dialog input[name='check-action-vda']:checked")
+        b.click(f"#vm-{name}-delete-modal-dialog button:contains(Delete)")
+        b.wait_not_present(f"#vm-{name}-delete-modal-dialog")
+
+        self.waitVmRow(name, "system", False)
+
+        # Guest disk image has been deleted
+        m.execute(f"while test -f {img2}; do sleep 1; done")
+
+        self.assertNotIn(name, m.execute("virsh list --all --name"))
+
         # Deleting a running guest will disconnect the serial console
         self.allow_browser_errors("Disconnection timed out.")
         self.allow_journal_messages(".* couldn't shutdown fd: Transport endpoint is not connected")