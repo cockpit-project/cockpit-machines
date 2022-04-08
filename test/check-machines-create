cockpit-project/cockpit-machines

commit 5d7bbab3cda82479bfd734c444048b607fd559cb

Work around virt-install mis-detecting SPICE graphics support
Currently, spice graphics support requires QXL graphics, mostly
accidentally [1]. When uninstalling the qemu-device-display-qxl package,
`virsh domcapabilities` still includes "spice", but creating the domain
fails with:

    unsupported configuration: domain configuration does not support video model 'qxl'

Work around that by explicitly asking QEMU whether it supports QXL, and
disable spice if not. Only check qemu-system-x86_84, and ignore other
architectures for the time being. Spice is being deprecated anyway.

[1] https://bugzilla.redhat.com/show_bug.cgi?id=2064594

test/check-machines-create
@@ -545,8 +545,14 @@ vnc_password= "{vnc_passwd}"
                                                       start_vm=True))
 
     def testCreateUrlSource(self):
+        m = self.machine
         runner = TestMachinesCreate.CreateVmRunner(self)
         config = TestMachinesCreate.TestCreateConfig
+
+        # disable QXL driver for Fedora, which builds this as a module -- should work and not add a spice console then
+        if 'fedora' in m.image:
+            m.execute("mount -o bind /dev/null /usr/lib64/qemu/hw-display-qxl.so")
+            self.addCleanup(m.execute, "umount /usr/lib64/qemu/hw-display-qxl.so")
 
         self.login_and_go("/machines")
         self.browser.wait_in_text("body", "Virtual machines")
@@ -566,7 +572,7 @@ vnc_password= "{vnc_passwd}"
         runner.checkDialogFormValidationTest(TestMachinesCreate.VmDialog(self, "existing-name", storage_size=1,
                                                                          env_is_empty=False), {"vm-name": "already exists"})
 
-        self.machine.execute("virsh undefine existing-name")
+        m.execute("virsh undefine existing-name")
 
         runner.createTest(TestMachinesCreate.VmDialog(self, sourceType='url',
                                                       location=config.TREE_URL,
@@ -596,9 +602,9 @@ vnc_password= "{vnc_passwd}"
 
         # This functionality works on debian only because of extra qemu-block-extra dep.
         # Check error is returned if dependency is missing
-        if self.machine.image.startswith("debian"):
-            self.machine.execute("mount -o bind /dev/null /usr/lib/x86_64-linux-gnu/qemu/block-curl.so")
-            self.addCleanup(self.machine.execute, "umount /usr/lib/x86_64-linux-gnu/qemu/block-curl.so")
+        if m.image.startswith("debian"):
+            m.execute("mount -o bind /dev/null /usr/lib/x86_64-linux-gnu/qemu/block-curl.so")
+            self.addCleanup(m.execute, "umount /usr/lib/x86_64-linux-gnu/qemu/block-curl.so")
             runner.checkDialogErrorTest(TestMachinesCreate.VmDialog(self, sourceType='url',
                                                                     location=config.ISO_URL,
                                                                     memory_size=128, memory_size_unit='MiB',

src/scripts/install_machine.py
@@ -39,6 +39,21 @@ def get_graphics_capabilies(connection):
         for value in graphics.find('enum').findall('value'):
             consoles.append(value.text)
 
+    # HACK: spice requires QXL driver which is modular in Fedora https://bugzilla.redhat.com/show_bug.cgi?id=2064594
+    if 'spice' in consoles:
+        try:
+            out = subprocess.check_output(['qemu-system-x86_64', '-vga', 'help'])
+            for line in out.splitlines():
+                if line.startswith(b'qxl'):
+                    logging.debug('get_graphics_capabilies: qemu-system-x86_84 -vga help confirmed QXL support')
+                    break
+            else:
+                logging.debug('get_graphics_capabilies: qemu-system-x86_84 -vga help shows no QXL support, disabling spice')
+                consoles.remove('spice')
+        except (FileNotFoundError, subprocess.CalledProcessError) as e:
+            # non-x86 architectures/weird setups
+            logging.debug('get_graphics_capabilies: Querying qemu-system-x86_84 -vga help failed: %s', str(e))
+
     logging.debug('get_graphics_capabilies: ' + ', '.join(consoles))
 
     return [c for c in consoles if c in ['vnc', 'spice']]

commit 64b4bb7c7af8553295d0036d676b78281a1c74a4

test: Adju testCreateAndVerifyQemuConf() for spice/QXL driver bug
Commit 5d7bbab3cda introduced a hack for spice depending on the QXL
video driver (see https://bugzilla.redhat.com/show_bug.cgi?id=2064594)
Apply the same hack to testCreateAndVerifyQemuConf -- if the QXL driver
is not installed, then the generated XML will also not have spice
graphics.

Tighten the test to also assert that the spice driver is *not* present
when we don't expect it.

test/check-machines-create
@@ -862,17 +862,27 @@ vnc_password= "{vnc_passwd}"
             self.assertIn(f"backing file: {self.location}", self.machine.execute(f"qemu-img info /var/lib/libvirt/images/{self.name}.qcow2"))
 
         def createRespectQemuConfConsoleConfig(self, vnc_listen, spice_listen, vnc_passwd, spice_passwd):
+            m = self.machine
+
             self.browser.click(".pf-c-modal-box__footer button:contains(Create)")
             self.browser.wait_not_present("#create-vm-dialog")
             self.browser.wait_text(f"#vm-{self.name}-state", "Shut off")
 
-            domainXML = self.machine.execute(f"virsh dumpxml --security-info {self.name}")
+            domainXML = m.execute(f"virsh dumpxml --security-info {self.name}")
             root = ET.fromstring(domainXML)
 
-            if self.machine.image not in ["rhel-9-0", "centos-9-stream"]:
-                spice = root.findall(".//graphics[@type='spice']")[0]
-                self.assertEqual(spice_listen, spice.attrib['listen'])
-                self.assertEqual(spice_passwd, spice.attrib.get('passwd', None))
+            expect_spice = m.image not in ["rhel-9-0", "centos-9-stream"]
+            if m.image.startswith("fedora"):
+                # HACK: Fedora splits out the driver into a separate package, which we don't depend on
+                # spice currently requires this, but shouldn't: https://bugzilla.redhat.com/show_bug.cgi?id=2064594
+                expect_spice = m.execute("if rpm -q qemu-device-display-qxl >/dev/null; then echo yes; fi").strip() == "yes"
+
+            spice = root.findall(".//graphics[@type='spice']")
+            if expect_spice:
+                self.assertEqual(spice_listen, spice[0].attrib['listen'])
+                self.assertEqual(spice_passwd, spice[0].attrib.get('passwd', None))
+            else:
+                self.assertEqual(spice, [])
 
             vnc = root.findall(".//graphics[@type='vnc']")[0]
             self.assertEqual(vnc_listen, vnc.attrib['listen'])