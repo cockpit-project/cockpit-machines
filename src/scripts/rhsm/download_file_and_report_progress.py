9f8e28ee41893fd4c007ba29fd6f503956416c4a
Simon Kobyda <skobyda@redhat.com>
2022-05-10 15:33:54 +0200

    Add scripts for downloading rhel images
    Firstly we get an access token will is used for accessing RHSM APIs.
    To get an access token, we need offline Token, which user can retrieve
    from Red Hat Customer Portal.
    Then we call /images/rhel/{Version}/{Arch} with access token as bearer
    token to get url of specific RHEL image.
    Then we use the url download the image to a server.

---
+++ src/scripts/rhsm/download_file_and_report_progress.py
@@ -0,0 +1,32 @@
+import urllib.request
+import urllib.error
+import sys
+import json
+import os
+
+args = json.loads(sys.argv[1], strict=False)
+
+download_path = os.path.join(args['downloadDir'], args['fileName'])
+if not os.path.exists(download_path):
+    # Create download directory, don't complain when it already exists
+    os.makedirs(args['downloadDir'], exist_ok=True)
+
+    try:
+        req = urllib.request.Request(args['url'])
+        req.add_header("Authorization", f"Bearer {args['accessToken']}")
+        with urllib.request.urlopen(req) as s, open(download_path, "wb") as f:
+            size = int(s.headers["content-length"])
+            done = 0
+            percentage = -1
+            block = s.read(2 ** 20)
+            while block:
+                done += len(block)
+                new_percentage = round(done * 100 / size)
+                if (new_percentage > percentage):
+                    percentage = new_percentage
+                    print(f"{percentage}", flush=True)  # print adds newling by default, which serves nicely as a delimeter
+                f.write(block)
+                block = s.read(2 ** 20)
+
+    except urllib.error.URLError as error:
+        sys.exit(error)
---
+++ src/scripts/rhsm/get_access_token.py
@@ -0,0 +1,27 @@
+import urllib.request
+import urllib.error
+import sys
+import json
+
+args = json.loads(sys.argv[1], strict=False)
+
+data = {
+    "grant_type": "refresh_token",
+    "client_id": "rhsm-api",
+    "refresh_token": args["offlineToken"]
+}
+try:
+    req = urllib.request.Request("https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token",
+                                 urllib.parse.urlencode(data).encode(),
+                                 headers={"Content-Type": "application/x-www-form-urlencoded"})
+    with urllib.request.urlopen(req) as s:
+        response_body = s.read().decode()
+        ret_obj = json.loads(response_body)
+
+        # Handle RHSM API failure
+        if "error" in ret_obj:
+            sys.exit(ret_obj["error"])
+
+    print(ret_obj["access_token"])
+except Exception as error:
+    sys.exit(error)
