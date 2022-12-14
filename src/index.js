bc7af4d4cfa01c9d1b440febb3871360f731ffe0
Katerina Koukiou <kkoukiou@redhat.com>
2022-12-14 17:27:25 +0100

    Adjust dark theme class and bring in tabular fonts
    Also bump cockpit repo commit for fetching lib

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
+import "cockpit-dark-theme";
 import 'patternfly/patternfly-4-cockpit.scss';
 import 'polyfills'; // once per application
 
