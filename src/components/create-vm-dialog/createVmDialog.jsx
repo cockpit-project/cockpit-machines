8259183278e83220c14f6c91f16bad5796582953
Simon Kobyda <skobyda@redhat.com>
2023-04-21 12:53:09 +0200

    Fix FormHelper of "Storage" and "Memory" when variant is warning
    And add matching test

--- src/components/create-vm-dialog/createVmDialog.jsx
+++ src/components/create-vm-dialog/createVmDialog.jsx
@@ -808,6 +808,7 @@ const MemoryRow = ({ memorySize, memorySizeUnit, nodeMaxMemory, minimumMemory, o
                     </FormSelect>
                 </InputGroup>
                 <FormHelper fieldId="memory-size"
+                            variant={validationStateMemory}
                             helperTextInvalid={validationStateMemory == "error" && validationFailed.memory}
                             helperText={helperText} />
             </FormGroup>
@@ -932,6 +933,7 @@ const StorageRow = ({ connectionName, allowNoDisk, storageSize, storageSizeUnit,
                     </InputGroup>
                     <FormHelper
                         fieldId="storage-limit"
+                        variant={validationStateStorage}
                         helperTextInvalid={validationStateStorage == "error" && validationFailed.storage}
                         helperText={helperTextNewVolume} />
                 </FormGroup>
