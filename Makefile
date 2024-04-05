cockpit-project/cockpit-machines

commit 5ec3a27f1fb417dc38032ab3579edd37cf62afc8

Use Cockpit's KebabDropdown component

src/components/vm/disks/vmDiskColumns.jsx
@@ -22,8 +22,10 @@ import cockpit from 'cockpit';
 
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm } from "@patternfly/react-core/dist/esm/components/DescriptionList";
-import { Dropdown, DropdownItem, KebabToggle } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
+import { DropdownItem } from "@patternfly/react-core/dist/esm/components/Dropdown";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
+
+import { KebabDropdown } from 'cockpit-components-dropdown.jsx';
 
 import { useDialogs } from 'dialogs.jsx';
 import { domainDeleteStorage, domainDetachDisk, domainGet } from '../../../libvirtApi/domain.js';
@@ -235,14 +237,12 @@ export const DiskActions = ({ vm, vms, disk, supportedDiskBusTypes, idPrefixRow,
                             vm={vm}
                             idPrefix={`${idPrefixRow}-edit`}
                             supportedDiskBusTypes={supportedDiskBusTypes} />}
-            <Dropdown onSelect={() => setIsActionOpen(false)}
-                      key={idPrefixRow + "-action-kebab"}
-                      id={idPrefixRow + "-action-kebab"}
-                      toggle={<KebabToggle onToggle={(_event, isOpen) => setIsActionOpen(isOpen)} />}
-                      isPlain
-                      isOpen={isActionOpen}
-                      position='right'
-                      dropdownItems={dropdownItems} />
+            <KebabDropdown
+                key={idPrefixRow + "-action-kebab"}
+                toggleButtonId={idPrefixRow + "-action-kebab"}
+                position='right'
+                dropdownItems={dropdownItems}
+            />
         </div>
     );
 };

src/components/vm/nics/vmNicsCard.jsx
@@ -20,7 +20,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm } from "@patternfly/react-core/dist/esm/components/DescriptionList";
-import { Dropdown, KebabToggle } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
 import { Flex, FlexItem } from "@patternfly/react-core/dist/esm/layouts/Flex";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
 import { DialogsContext } from 'dialogs.jsx';
@@ -32,6 +31,7 @@ import { EditNICModal } from './nicEdit.jsx';
 import { needsShutdownIfaceModel, needsShutdownIfaceSource, needsShutdownIfaceType, NeedsShutdownTooltip } from '../../common/needsShutdown.jsx';
 import './nic.css';
 import { domainChangeInterfaceSettings, domainDetachIface, domainInterfaceAddresses, domainGet } from '../../../libvirtApi/domain.js';
+import { KebabDropdown } from "cockpit-components-dropdown";
 import { ListingTable } from "cockpit-components-table.jsx";
 import { DeleteResourceButton } from '../../common/deleteResource.jsx';
 
@@ -493,16 +493,6 @@ export class VmNetworkTab extends React.Component {
                         );
                     }
 
-                    const isOpen = this.state.dropdownOpenActions.has(network.mac);
-                    const setIsOpen = open => {
-                        const next = new Set(this.state.dropdownOpenActions);
-                        if (open)
-                            next.add(network.mac);
-                        else
-                            next.delete(network.mac);
-
-                        this.setState({ dropdownOpenActions: next });
-                    };
                     return (
                         <div className='machines-listing-actions'>
                             <Button id={`${id}-iface-${networkId}-` + (isUp ? 'unplug' : 'plug')}
@@ -511,14 +501,7 @@ export class VmNetworkTab extends React.Component {
                                 {isUp ? 'Unplug' : 'Plug'}
                             </Button>
                             {editNICAction()}
-                            <Dropdown onSelect={() => setIsOpen(false)}
-                                id={`${id}-iface-${networkId}-action-kebab`}
-                                key={`${id}-iface-${networkId}-action-kebab`}
-                                toggle={<KebabToggle onToggle={(_event, isDropdownOpen) => setIsOpen(isDropdownOpen)} />}
-                                isPlain
-                                isOpen={isOpen}
-                                position='right'
-                                dropdownItems={[deleteButton]} />
+                            <KebabDropdown position="right" toggleButtonId={`${id}-iface-${networkId}-action-kebab`} dropdownItems={[deleteButton]} />
                         </div>
                     );
                 },

src/components/common/deleteResource.jsx
@@ -21,7 +21,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { DescriptionList, DescriptionListDescription, DescriptionListGroup, DescriptionListTerm } from "@patternfly/react-core/dist/esm/components/DescriptionList";
-import { DropdownItem } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
+import { DropdownItem } from "@patternfly/react-core/dist/esm/components/Dropdown";
 import { Modal } from "@patternfly/react-core/dist/esm/components/Modal";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
 

src/components/networks/network.jsx
@@ -18,7 +18,7 @@
  */
 import React, { useState } from 'react';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
-import { Dropdown, DropdownItem, KebabToggle } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
+import { DropdownItem } from "@patternfly/react-core/dist/esm/components/Dropdown";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
 import { useDialogs } from 'dialogs.jsx';
 
@@ -36,6 +36,8 @@ import {
     networkUndefine
 } from '../../libvirtApi/network.js';
 import store from '../../store.js';
+
+import { KebabDropdown } from "cockpit-components-dropdown";
 
 import cockpit from 'cockpit';
 
@@ -88,7 +90,6 @@ export const getNetworkRow = ({ network }) => {
 
 const NetworkActions = ({ network }) => {
     const Dialogs = useDialogs();
-    const [isActionOpen, setIsActionOpen] = useState(false);
     const [operationInProgress, setOperationInProgress] = useState(false);
 
     const onActivate = () => {
@@ -173,13 +174,7 @@ const NetworkActions = ({ network }) => {
                 {_("Activate")}
             </Button>
             }
-            <Dropdown onSelect={() => setIsActionOpen(!isActionOpen)}
-                      id={`${id}-action-kebab`}
-                      toggle={<KebabToggle onToggle={(_event, isOpen) => setIsActionOpen(isOpen)} />}
-                      isPlain
-                      isOpen={isActionOpen}
-                      position='right'
-                      dropdownItems={dropdownItems} />
+            <KebabDropdown position="right" toggleButtonId={`${id}-action-kebab`} dropdownItems={dropdownItems} />
         </div>
     );
 };

src/components/storagePools/storagePool.jsx
@@ -18,11 +18,11 @@
  */
 import React from 'react';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
-import { Dropdown, KebabToggle } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
 import { Progress } from "@patternfly/react-core/dist/esm/components/Progress";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
 
 import { ListingPanel } from 'cockpit-components-listing-panel.jsx';
+import { KebabDropdown } from "cockpit-components-dropdown";
 import {
     convertToUnit,
     rephraseUI,
@@ -196,13 +196,7 @@ class StoragePoolActions extends React.Component {
             <div className="btn-group">
                 { storagePool.active && deactivateButton }
                 { !storagePool.active && activateButton }
-                <Dropdown onSelect={() => this.setState(prevState => ({ isActionOpen: !prevState.isActionOpen }))}
-                          id={`${id}-action-kebab`}
-                          toggle={<KebabToggle onToggle={(_event, isActionOpen) => this.setState({ isActionOpen })} />}
-                          isPlain
-                          isOpen={this.state.isActionOpen}
-                          position='right'
-                          dropdownItems={dropdownItems} />
+                <KebabDropdown toggleButtonId={`${id}-action-kebab`} position='right' dropdownItems={dropdownItems} />
             </div>
         );
     }

src/components/storagePools/storagePoolDelete.jsx
@@ -20,7 +20,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
 import { Checkbox } from "@patternfly/react-core/dist/esm/components/Checkbox";
-import { DropdownItem } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
+import { DropdownItem } from "@patternfly/react-core/dist/esm/components/Dropdown";
 import { HelperText, HelperTextItem } from "@patternfly/react-core/dist/esm/components/HelperText";
 import { List, ListItem } from "@patternfly/react-core/dist/esm/components/List";
 import { Modal } from "@patternfly/react-core/dist/esm/components/Modal";

src/components/vm/vmActions.jsx
@@ -20,11 +20,13 @@ import cockpit from 'cockpit';
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from "@patternfly/react-core/dist/esm/components/Button";
-import { Dropdown, DropdownItem, DropdownSeparator, KebabToggle } from "@patternfly/react-core/dist/esm/deprecated/components/Dropdown";
+import { Divider } from "@patternfly/react-core/dist/esm/components/Divider";
+import { DropdownItem } from "@patternfly/react-core/dist/esm/components/Dropdown";
 import { Tooltip } from "@patternfly/react-core/dist/esm/components/Tooltip";
 import { PowerOffIcon, RedoIcon } from '@patternfly/react-icons';
 import { useDialogs } from 'dialogs.jsx';
 import { fmt_to_fragments } from 'utils.jsx';
+import { KebabDropdown } from 'cockpit-components-dropdown.jsx';
 
 import { updateVm } from '../../actions/store-actions.js';
 import {
@@ -180,7 +182,6 @@ const onSendNMI = (vm) => domainSendNMI({ name: vm.name, id: vm.id, connectionNa
 
 const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
     const Dialogs = useDialogs();
-    const [isActionOpen, setIsActionOpen] = useState(false);
     const [operationInProgress, setOperationInProgress] = useState(false);
     const [prevVmState, setPrevVmState] = useState(vm.state);
     const [virtCloneAvailable, setVirtCloneAvailable] = useState(false);
@@ -210,7 +211,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Pause")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-pause" />);
+        dropdownItems.push(<Divider key="separator-pause" />);
     }
 
     if (domainCanResume(state)) {
@@ -221,7 +222,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Resume")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-resume" />);
+        dropdownItems.push(<Divider key="separator-resume" />);
     }
 
     if (domainCanShutdown(state)) {
@@ -300,7 +301,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Force shut down")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-shutdown" />);
+        dropdownItems.push(<Divider key="separator-shutdown" />);
         dropdownItems.push(
             <DropdownItem key={`${id}-sendNMI`}
                           id={`${id}-sendNMI`}
@@ -320,7 +321,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Send non-maskable interrupt")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-sendnmi" />);
+        dropdownItems.push(<Divider key="separator-sendnmi" />);
     }
 
     if (domainCanReset(state)) {
@@ -364,7 +365,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Force reboot")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-reset" />);
+        dropdownItems.push(<Divider key="separator-reset" />);
     }
 
     let run = null;
@@ -426,7 +427,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Migrate")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-migrate" />);
+        dropdownItems.push(<Divider key="separator-migrate" />);
     }
 
     if (vm.inactiveXML?.hasSpice) {
@@ -437,7 +438,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Replace SPICE devices")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-spice" />);
+        dropdownItems.push(<Divider key="separator-spice" />);
     }
 
     if (domainCanRename(state)) {
@@ -450,7 +451,7 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
                 {_("Rename")}
             </DropdownItem>
         );
-        dropdownItems.push(<DropdownSeparator key="separator-rename" />);
+        dropdownItems.push(<Divider key="separator-rename" />);
     }
 
     if (state !== undefined && domainCanDelete(state, vm.id)) {
@@ -479,13 +480,11 @@ const VmActions = ({ vm, vms, onAddErrorNotification, isDetailsPage }) => {
             {run}
             {shutdown}
             {install}
-            <Dropdown onSelect={() => setIsActionOpen(!isActionOpen)}
-                      id={`${id}-action-kebab`}
-                      toggle={<KebabToggle isDisabled={vm.isUi} onToggle={(_event, isOpen) => setIsActionOpen(isOpen)} />}
-                      isPlain
-                      isOpen={isActionOpen}
-                      position='right'
-                      dropdownItems={dropdownItems} />
+            <KebabDropdown
+                toggleButtonId={`${id}-action-kebab`}
+                position='right'
+                dropdownItems={dropdownItems}
+            />
         </div>
     );
 };

src/machines.scss
@@ -44,18 +44,11 @@
     margin-inline: 0;
 }
 
-.machines-listing-actions button:not(.pf-v5-c-dropdown__toggle) {
+.machines-listing-actions button:not(.pf-v5-c-menu-toggle) {
     margin-block: 0.25rem;
     margin-inline: 0.5rem 0;
 }
 
-.machines-listing-actions > .pf-v5-c-dropdown {
-    margin-block-start: 0.25rem;
-}
-
-.pf-v5-c-dropdown__menu-item.pf-m-danger {
-    color: var(--pf-v5-global--danger-color--200);
-}
 
 .pf-v5-c-table {
     .btn-group {

commit 42933ae1b93b463236e03e9a1632a5c1cb6f2cf6

Go back to external "open" state management for KebabDropdown
Commit 5ec3a27f1fb417dc3 introduced a race condition: kebabps rendered
as part of a list are sometimes not persistent. This leads to
immediately closing the kebab again when the user/test opens it if the
table changes at the same time (most prominently in
`TestMachinesDisks.testDetachDisk`).

This can only be fixed with managing the state outside of the list, as
we did before 5ec3a27f1fb417dc3. All that state was still present in
`DiskActions`, so use the new API from
https://github.com/cockpit-project/cockpit/pull/20265 to just pass in
the existing external state management.

For `VmNetworkTab`, bring back the `dropdownOpenActions` (which still
existed as state) translation into a per-interface property from
5ec3a27f1fb417dc3.

`VmActions` and `NetworkActions` already tracked the state inside the
tables, so either they were already broken before 5ec3a27f1fb417dc3 or
are fine.

Makefile
@@ -41,7 +41,7 @@ COCKPIT_REPO_FILES = \
 	$(NULL)
 
 COCKPIT_REPO_URL = https://github.com/cockpit-project/cockpit.git
-COCKPIT_REPO_COMMIT = 4c4356e4ddbd9e716b5f364a5c4fed748589595a # 314 + 7 commits
+COCKPIT_REPO_COMMIT = 71162c02cdf2459c0251ae2261a385a548b4e0ea  # 314 + 12 commits
 
 $(COCKPIT_REPO_FILES): $(COCKPIT_REPO_STAMP)
 COCKPIT_REPO_TREE = '$(strip $(COCKPIT_REPO_COMMIT))^{tree}'

src/components/vm/disks/vmDiskColumns.jsx
@@ -242,6 +242,8 @@ export const DiskActions = ({ vm, vms, disk, supportedDiskBusTypes, idPrefixRow,
                 toggleButtonId={idPrefixRow + "-action-kebab"}
                 position='right'
                 dropdownItems={dropdownItems}
+                isOpen={isActionOpen}
+                setIsOpen={setIsActionOpen}
             />
         </div>
     );

src/components/vm/nics/vmNicsCard.jsx
@@ -493,6 +493,17 @@ export class VmNetworkTab extends React.Component {
                         );
                     }
 
+                    const isOpen = this.state.dropdownOpenActions.has(network.mac);
+                    const setIsOpen = open => {
+                        const next = new Set(this.state.dropdownOpenActions);
+                        if (open)
+                            next.add(network.mac);
+                        else
+                            next.delete(network.mac);
+
+                        this.setState({ dropdownOpenActions: next });
+                    };
+
                     return (
                         <div className='machines-listing-actions'>
                             <Button id={`${id}-iface-${networkId}-` + (isUp ? 'unplug' : 'plug')}
@@ -501,7 +512,11 @@ export class VmNetworkTab extends React.Component {
                                 {isUp ? 'Unplug' : 'Plug'}
                             </Button>
                             {editNICAction()}
-                            <KebabDropdown position="right" toggleButtonId={`${id}-iface-${networkId}-action-kebab`} dropdownItems={[deleteButton]} />
+                            <KebabDropdown position="right"
+                                           toggleButtonId={`${id}-iface-${networkId}-action-kebab`}
+                                           dropdownItems={[deleteButton]}
+                                           isOpen={isOpen}
+                                           setIsOpen={setIsOpen} />
                         </div>
                     );
                 },