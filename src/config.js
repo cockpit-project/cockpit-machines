2b0c5b8103e78d1eb0f5cfede0263d6d5f03f28c
Marius Vollmer <mvollmer@redhat.com>
2024-04-17 17:11:32 +0300

    config: Load configuration values from manifest
    And use that mechanism to control storage during migration.

--- src/config.js
+++ src/config.js
@@ -17,9 +17,11 @@
  * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
  */
 
+import cockpit from 'cockpit';
+import { read_os_release } from "os-release.js";
+
 /**
  * Application-wide constants
- * TODO: make this configurable by user
  */
 const VMS_CONFIG = {
     DefaultRefreshInterval: 10000, // in ms
@@ -35,6 +37,34 @@ const VMS_CONFIG = {
             }
         }
     },
+
+    StorageMigrationSupported: true,
 };
+
+function try_fields(dict, fields, def) {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] && dict[fields[i]] !== undefined)
+            return dict[fields[i]];
+    }
+    return undefined;
+}
+
+export async function load_config() {
+    const config = cockpit.manifests.machines?.config;
+    const os_release = await read_os_release();
+
+    function get_config(name) {
+        if (config) {
+            let val = config[name];
+            if (typeof val === 'object' && val !== null)
+                val = try_fields(val, [os_release.PLATFORM_ID, os_release.ID]);
+            if (val !== undefined)
+                VMS_CONFIG[name] = val;
+        }
+    }
+
+    for (const f in VMS_CONFIG)
+        get_config(f);
+}
 
 export default VMS_CONFIG;
