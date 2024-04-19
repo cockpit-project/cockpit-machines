6b697284a2c6ce289f8a4fbc5ade743c067aec30
Marius Vollmer <mvollmer@redhat.com>
2024-04-19 12:59:06 +0300

    snapshots: Only use external snapshots for disks of type "file"
    The previous method of changing the source of a "volume" disk to be a
    "file" disk leads to broken snapshots that can't be reverted or
    deleted.
    
    And leaving the source alone leads to an error during snapshot
    creation.
    
    Fixes: https://issues.redhat.com/browse/RHEL-33468

--- src/libvirt-xml-create.js
+++ src/libvirt-xml-create.js
@@ -1,4 +1,21 @@
-import { getStoragePoolPath } from "./helpers.js";
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
 
 export function getDiskXML(type, file, device, poolName, volumeName, format, target, cacheMode, shareable, busType, serial) {
     const doc = document.implementation.createDocument('', '', null);
@@ -253,16 +270,6 @@ export function getSnapshotXML(name, description, disks, memoryPath, isExternal,
                 const diskElem = doc.createElement('disk');
                 diskElem.setAttribute('name', disk.target);
                 diskElem.setAttribute('snapshot', 'external');
-
-                if (disk.type === "volume") {
-                    const poolPath = getStoragePoolPath(storagePools, disk.source.pool, connectionName);
-                    if (poolPath) {
-                        const sourceElem = doc.createElement('source');
-                        sourceElem.setAttribute('file', `${poolPath}/${disk.source.volume}.snap`);
-                        diskElem.appendChild(sourceElem);
-                    }
-                }
-
                 disksElem.appendChild(diskElem);
             }
         });
