95f8b3a548daf5be323f9dcd60ff8b4be5b5977d
Marius Vollmer <marius.vollmer@gmail.com>
2025-04-01 14:38:32 +0300

    consoles: Redesign and reimplement
    - A ToggleGroup in the Card header is used to switch consoles
    
    - The console switcher is also there for shut-off machines. This gives
      us a place for type-specific actions that also make sense for a
      shut-off machine, like editing VNC server settings.
    
    - The DesktopViewer is gone, but there is a footer with a "Launch
      viewer" button and a "How to connect" popover.
    
    - Actions for the Graphics and Serial consoles are collected into
      kebab menus.
    
    - The expanded console has less UI around it, and it keeps the type
      that was active in the collapsed view. Instead of the breadcrumb it
      has a "Collapse" button.
    
    - When there is no actual console for a given type, there is now a
      EmptyState component where you can enable it.
    
    - It is possible to change VNC server settings via the "How to
      connect" popup.
    
    - The SPICE console invites you to replace it with VNC.
    
    - The size of the expanded console is now always controlled by the
      browser window and never overflows in height.
    
    - The VncConsole has been imported from @patternfly/react-console and
      stripped down.

---
+++ src/components/vm/consoles/spice.jsx
@@ -0,0 +1,94 @@
+/*
+ * This file is part of Cockpit.
+ *
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Cockpit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import cockpit from 'cockpit';
+
+import { Button } from "@patternfly/react-core/dist/esm/components/Button";
+import { EmptyState, EmptyStateBody, EmptyStateFooter, EmptyStateActions } from "@patternfly/react-core/dist/esm/components/EmptyState";
+import { Split, SplitItem } from "@patternfly/react-core/dist/esm/layouts/Split/index.js";
+
+import { useDialogs } from 'dialogs';
+
+import { ReplaceSpiceDialog } from '../vmReplaceSpiceDialog.jsx';
+import { RemoteConnectionPopover, connection_address, console_launch } from './common';
+
+const _ = cockpit.gettext;
+
+const SpiceFooter = ({ vm, spice }) => {
+    return (
+        <div className="vm-console-footer">
+            <Split>
+                <SplitItem isFilled />
+                <SplitItem>
+                    <RemoteConnectionPopover
+                        url={spice && cockpit.format("spice://$0:$1", connection_address(), spice.port)}
+                    />
+                    <Button
+                        variant="secondary"
+                        onClick={() => console_launch(vm, spice)}
+                        isDisabled={!spice}
+                    >
+                        {_("Launch viewer")}
+                    </Button>
+                </SplitItem>
+            </Split>
+        </div>
+    );
+};
+
+const Spice = ({ vm, isActive, spice }) => {
+    const Dialogs = useDialogs();
+
+    function replace_spice() {
+        Dialogs.show(<ReplaceSpiceDialog vm={vm} vms={[vm]} />);
+    }
+
+    return (
+        <>
+            <EmptyState>
+                <EmptyStateBody>
+                    {_("This machine has a SPICE graphical console that can not be shown here.")}
+                    { !isActive &&
+                        _(" Start the virtual machine to be able to launch a remote viewer for this console")
+                    }
+                </EmptyStateBody>
+                <EmptyStateFooter>
+                    <EmptyStateActions>
+                        <Button
+                            variant="link"
+                            onClick={replace_spice}
+                        >
+                            {_("Replace with VNC")}
+                        </Button>
+                    </EmptyStateActions>
+                </EmptyStateFooter>
+            </EmptyState>
+            <SpiceFooter vm={vm} spice={spice} />
+        </>
+    );
+};
+
+export const SpiceActive = ({ vm, spice }) => {
+    return <Spice isActive vm={vm} spice={spice} />;
+};
+
+export const SpiceInactive = ({ vm }) => {
+    return <Spice vm={vm} />;
+};
