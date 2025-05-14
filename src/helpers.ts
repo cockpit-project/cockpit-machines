95e4cd8c4ba2935315042cbf1f2ac5f660d63bcd
Marius Vollmer <marius.vollmer@gmail.com>
2025-05-14 15:34:03 +0300

    common, vms: Add TypeScript

--- src/helpers.ts
+++ src/helpers.ts
@@ -23,21 +23,18 @@ import type {
     optString,
     ConnectionName,
     VM, VMState, VMDisk, VMInterface, VMRedirectedDevice, VMHostDevice,
+    UIVM,
     NodeDevice,
     StoragePool,
     HypervisorCapabilities,
 } from './types';
-
-import type {
-    UIVMState
-} from './components/create-vm-dialog/uiState';
 
 const _ = cockpit.gettext;
 
 export const LIBVIRT_SESSION_CONNECTION = 'session';
 export const LIBVIRT_SYSTEM_CONNECTION = 'system';
 
-export function dummyVmsFilter(vms: VM[], uiVms: UIVMState[]): UIVMState[] {
+export function dummyVmsFilter(vms: VM[], uiVms: UIVM[]): UIVM[] {
     return uiVms.filter(uiVm => vms.find(vm => vm.name == uiVm.name && vm.connectionName == uiVm.connectionName) === undefined);
 }
 
@@ -920,7 +917,7 @@ export function getVmStoragePools(connectionName: ConnectionName): StoragePool[]
     return storagePools.filter(sp => sp && sp.name && sp.connectionName == connectionName && sp.active);
 }
 
-export function nicLookupByMAC(interfacesList: VMInterface[], mac: string): VMInterface {
+export function nicLookupByMAC(interfacesList: VMInterface[], mac: optString): VMInterface {
     return interfacesList.filter(iface => iface.mac == mac)[0];
 }
 
