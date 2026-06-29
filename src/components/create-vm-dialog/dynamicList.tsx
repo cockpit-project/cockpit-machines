3304febff1c1d101ff5c9c8f46cced87102b3c42
Marius Vollmer <marius.vollmer@gmail.com>
2026-06-29 16:35:55 +0300

    create: Rewrite the dialog using the dialog kit
    This mainly untangles the state handling, and splits out the code for
    the "Details" and "Automation" tabs.

---
+++ src/components/create-vm-dialog/dynamicList.tsx
@@ -0,0 +1,65 @@
+/*
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * Copyright (C) 2026 Red Hat, Inc.
+ */
+
+import cockpit from "cockpit";
+import React, { useId } from 'react';
+
+import { Button } from "@patternfly/react-core/dist/esm/components/Button";
+import { EmptyState, EmptyStateBody } from "@patternfly/react-core/dist/esm/components/EmptyState";
+import { FormFieldGroup, FormFieldGroupHeader } from "@patternfly/react-core/dist/esm/components/Form";
+
+import {
+    DialogField,
+    DialogHelperText,
+} from "cockpit/dialog";
+
+export function DynamicList<T>({
+    field,
+    label,
+    actionLabel,
+    emptyStateString,
+    render,
+    init,
+} : {
+    field: DialogField<T[]>,
+    label: string,
+    actionLabel: string,
+    emptyStateString: string,
+    render: (field: DialogField<T>, index: number) => React.ReactNode,
+    init: T,
+}) {
+    const id = useId();
+    return (
+        <FormFieldGroup
+            header={
+                <FormFieldGroupHeader
+                    titleText={{ text: label, id }}
+                    actions={
+                        <Button
+                            variant="secondary"
+                            className="btn-add"
+                            onClick={() => field.add(init)}
+                        >
+                            {actionLabel}
+                        </Button>
+                    }
+                />
+            }
+            className="dynamic-form-group"
+        >
+            {
+                field.get().length > 0
+                    ? field.map(render)
+                    : <EmptyState>
+                        <EmptyStateBody>
+                            {emptyStateString}
+                        </EmptyStateBody>
+                    </EmptyState>
+            }
+            <DialogHelperText field={field} />
+        </FormFieldGroup>
+    );
+}
