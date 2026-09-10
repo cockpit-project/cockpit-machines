2ee724a3e0a06da96c0dd941e472067a1e6373da
Marius Vollmer <marius.vollmer@gmail.com>
2026-09-10 09:37:17 +0300

    create: Rewrite the dialog using the dialog kit
    This mainly untangles the state handling, and splits out the code for
    the "Details" and "Automation" tabs.

--- src/components/create-vm-dialog/createVmDialogUtils.tsx
+++ src/components/create-vm-dialog/createVmDialogUtils.tsx
@@ -37,12 +37,14 @@ export const EDIT = 2;
  * Uses libosinfo to autodetect an OS based on its media/treeinfo.
  * treeinfo detection currently works only for rpm based distros.
  */
-export function autodetectOS(url: string) {
+export function autodetectOS(url: string, signal: AbortSignal) {
     // HACK: osinfo-detect uses GIO to read the tree info file over http. cockpit-bridge used to unset GIO env variables
     // which blocked us from using GIO calls over cockpit-bridge.
     // Overwrite the env vars here, until commit https://github.com/cockpit-project/cockpit/commit/86c1fcb46291c83d6c6903e60fe4bee82598d3a9
     // exists in all supported distros.
-    return python.spawn(autoDetectOSScript, [url], { environ: ['GIO_USE_VFS=gvfs', 'LC_ALL=C.UTF-8'], err: 'message' });
+    const channel = python.spawn(autoDetectOSScript, [url], { environ: ['GIO_USE_VFS=gvfs', 'LC_ALL=C.UTF-8'], err: 'message' });
+    signal.onabort = () => channel.close();
+    return channel;
 }
 
 export function getOSStringRepresentation(os: OSInfo) {
